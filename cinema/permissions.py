fe246c02fce1851393277a28e089d94535ab9193
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2026-01-05 23:46:58 +0200

    fixed code style

--- cinema/permissions.py
+++ cinema/permissions.py
@@ -5,4 +5,8 @@ class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
     def has_permission(self, request, view):
         if request.user and request.user.is_staff:
             return True
-        return request.user and request.user.is_authenticated and request.method in SAFE_METHODS
+        return (
+            request.user
+            and request.user.is_authenticated
+            and request.method in SAFE_METHODS
+        )
